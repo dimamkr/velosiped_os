e7c531094dd91df5c9092f9140e8dd7ad59e0b0f
dimamkr <makarovdmitriy2006@gmail.com>
2026-08-31 23:31:33 +0300

    improved paging

--- src/paging.h
+++ src/paging.h
@@ -2,11 +2,46 @@
 #define PAGING_H
 
 #include "types.h"
+#include "dynamic_array.h"
+
+// ВЕЗДЕ АДРЕС ВИРТУАЛЬНЫЙ ЕСЛИ НЕ УКАЗАНО ОБРАТНОЕ
+
+// Флаги страниц
+#define PAGE_PRESENT 0x1
+#define PAGE_RW 0x2
+#define PAGE_USER 0x4 // пользователь имеет доступ
+#define PAGE_WRITETHROUGH 0x8
+#define PAGE_CACHE_DISABLE 0x10
+#define PAGE_ACCESSED 0x20
+#define PAGE_DIRTY 0x40
+
+#define PAGE_KERNEL (PAGE_PRESENT | PAGE_RW) // страница ядра для ядра
 
 #define PAGE_SIZE 4096
 #define PAGE_DIR_ENTRIES 1024
 #define PAGE_TABLE_ENTRIES 1024
 
-void init_paging(void);
+extern void enable_paging(void);
+extern void paging_load_directory(uint32_t);
+
+typedef struct
+{
+    uint32_t data[PAGE_TABLE_ENTRIES] __attribute__((aligned(4096)));
+} page_container;
+
+typedef struct
+{
+    page_container *page_dir;     // вирт адрес директории
+    dynamic_array_t *page_tables; // хранит вирт адреса таблиц
+} page_dict;
+
+// Создание нового пустого каталога
+page_dict *page_dict_create(void);
+
+void page_dict_map_page(page_dict *pd, uint32_t virt_addr, uint32_t flags);
+void page_dict_unmap_page(page_dict *pd, uint32_t virt_addr);
+void page_dict_destroy(page_dict *pd);
+void page_dict_map_page_to_phys(page_dict *pd, uint32_t virt_addr, uint32_t phys_addr, uint32_t flags);
+void page_dict_switch(page_dict *pd);
 
 #endif
---
+++ src/pmm.h
@@ -0,0 +1,12 @@
+// physical memory manager
+#ifndef PMM_H
+#define PMM_H
+
+#include "types.h"
+
+void pmm_init(void);
+uint32_t pmm_alloc_frame();
+void pmm_free_frame(uint32_t phys_addr);
+void pmm_set_alloced_flag(uint32_t phys_addr);
+
+#endif
