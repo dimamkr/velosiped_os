b4b6c3ee453b77dc5b9a757b5cffa3586e7fe8e9
dimamkr <makarovdmitriy2006@gmail.com>
2026-09-02 00:23:49 +0300

    added TASK_LOCKED_FUNCTION macro & fixed heap destruction bug

--- src/task_event.c
+++ src/task_event.c
@@ -3,22 +3,26 @@
 
 extern task_t tasks[];
 
-void task_event_init(task_event_t *this)
+task_event_t *task_event_create()
 {
-    this->front = NULL;
+    task_event_t *t = malloc(sizeof(task_event_t));
+    t->front = NULL;
+    return t;
 }
 
 void task_event_add(task_event_t *this, uint32_t pid)
 {
-    task_lock();
+    TASK_LOCKED_FUNCTION;
+
     tasks[pid].state = TASK_WAITING;
 
     linked_list_add_begin(&(this->front), &pid, sizeof(uint32_t));
-    task_unlock();
 }
 
 void task_event_flush(task_event_t *this)
 {
+    TASK_LOCKED_FUNCTION;
+
     while (this->front)
     {
         uint32_t pid = *(uint32_t *)this->front->value;
