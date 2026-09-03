2d14f7628de280e3ebc2dfa4a627cd3aa5f65d06
dimamkr <makarovdmitriy2006@gmail.com>
2026-09-03 22:52:03 +0300

    added copy

--- src/dynamic_array.h
+++ src/dynamic_array.h
@@ -26,6 +26,7 @@ void dynamic_array_pop_back(dynamic_array_t *array);
 void dynamic_array_pop_front(dynamic_array_t *array);
 void dynamic_array_clear(dynamic_array_t *array);
 void dynamic_array_quicksort(dynamic_array_t *array, uint32_t l_index, uint32_t r_index, dynamic_array_less_cb less);
+void dynamic_array_copy(dynamic_array_t *dst, dynamic_array_t *src);
 
 #define dynamic_array_get_by_index(array, index) ((void *)((array)->buffer + (((array)->start + (index) + 1) & ((array)->size - 1)) * (array)->size_of_element))
 #define dynamic_array_set_by_index(array, index, value) (memcpy(dynamic_array_get_by_index(array, index), value, (array)->size_of_element))
