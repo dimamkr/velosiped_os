d558b5ac6aaafb0dc2147f65ec009a4435cd9f12
dimamkr <makarovdmitriy2006@gmail.com>
2026-09-14 23:15:09 +0300

    added rgba

--- src/colors.c
+++ src/colors.c
@@ -13,14 +13,14 @@ uint32_t color_blue;
 
 void colors_init(void)
 {
-    color_light_gray = color_to_pixel(170, 170, 170);
-    color_black = color_to_pixel(0, 0, 0);
-    color_light_green = color_to_pixel(0, 255, 0);
-    color_yellow = color_to_pixel(255, 255, 0);
-    color_red = color_to_pixel(170, 0, 0);
-    color_light_red = color_to_pixel(255, 85, 85);
-    color_white = color_to_pixel(255, 255, 255);
-    color_light_blue = color_to_pixel(85, 85, 255);
-    color_green = color_to_pixel(0, 255, 0);
-    color_blue = color_to_pixel(0, 0, 255);
+    color_light_gray = RGB(170, 170, 170);
+    color_black = RGB(0, 0, 0);
+    color_light_green = RGB(0, 255, 0);
+    color_yellow = RGB(255, 255, 0);
+    color_red = RGB(170, 0, 0);
+    color_light_red = RGB(255, 85, 85);
+    color_white = RGB(255, 255, 255);
+    color_light_blue = RGB(85, 85, 255);
+    color_green = RGB(0, 255, 0);
+    color_blue = RGB(0, 0, 255);
 }
--- src/colors.h
+++ src/colors.h
@@ -7,13 +7,58 @@
 
 #define SIZEOF_PIXEL sizeof(uint32_t)
 
-static inline uint32_t color_to_pixel(uint8_t r, uint8_t g, uint8_t b)
+// x от 1 до 65536
+#define DIV255(x) ((x + 1 + (x >> 8)) >> 8)
+
+static inline uint32_t color_to_pixel(uint8_t a, uint8_t r, uint8_t g, uint8_t b)
 {
     uint32_t color = 0;
+
     color |= ((uint32_t)(r >> (8 - fb.red_size))) << fb.red_pos;
     color |= ((uint32_t)(g >> (8 - fb.green_size))) << fb.green_pos;
     color |= ((uint32_t)(b >> (8 - fb.blue_size))) << fb.blue_pos;
+
+    color |= ((uint32_t)(a >> (8 - fb.alpha_size))) << fb.alpha_pos;
+
     return color;
+}
+
+static inline uint32_t color_rgb_add_a(uint32_t color, uint8_t a)
+{
+    color |= ((uint32_t)(a >> (8 - fb.alpha_size))) << fb.alpha_pos;
+}
+
+#define RGB(r, g, b) color_to_pixel(((1u << framebuffer.alpha_size) - 1), (r), (g), (b))
+#define RGBA(r, g, b, a) color_to_pixel((a), (r), (g), (b))
+#define RGB_TO_RGBA(color_rgb, a) color_rgb_add_a((color_rgb), (a))
+
+static inline uint32_t color_blend_pixels(uint32_t color_rgba, uint32_t color_rgb)
+{
+
+    uint32_t a = (color_rgba >> fb.alpha_pos) & ((1u << fb.alpha_size) - 1);
+
+    if (a == 255)
+        return color_rgba; // полностью непрозрачный
+    if (a == 0)
+        return color_rgb; // полностью прозрачный
+
+    // извлечение rgb
+    uint32_t sr = (color_rgba >> fb.red_pos) & ((1u << fb.red_size) - 1);
+    uint32_t sg = (color_rgba >> fb.green_pos) & ((1u << fb.green_size) - 1);
+    uint32_t sb = (color_rgba >> fb.blue_pos) & ((1u << fb.blue_size) - 1);
+
+    uint32_t dr = (color_rgb >> fb.red_pos) & ((1u << fb.red_size) - 1);
+    uint32_t dg = (color_rgb >> fb.green_pos) & ((1u << fb.green_size) - 1);
+    uint32_t db = (color_rgb >> fb.blue_pos) & ((1u << fb.blue_size) - 1);
+
+    // out = src * a/255 + dst * (1 - a/255)
+    uint32_t r = DIV255(sr * a + dr * (255 - a));
+    uint32_t g = DIV255(sg * a + dg * (255 - a));
+    uint32_t b = DIV255(sb * a + db * (255 - a));
+
+    uint32_t out = RGB(r, g, b);
+
+    return out;
 }
 
 #undef fb
