58c2e58b5a4c4c94f4876e7e8169debd3efcdfbf
dimamkr <makarovdmitriy2006@gmail.com>
2026-09-25 19:35:12 +0300

    more suitable USER_STACK_TOP

--- src/ram.h
+++ src/ram.h
@@ -28,7 +28,7 @@
 #define MMIO_VIRT_BASE 0xF0000000
 
 // пользовательские дефайны
-#define USER_STACK_TOP 0x7FFFF000
+#define USER_STACK_TOP 0xBFFFF000
 
 static inline void *ram_kernel_to_virt(void *phys_addr)
 {
